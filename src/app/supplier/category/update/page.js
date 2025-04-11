b956d763e83d62c96b640a780f961f83e5964e64
rohitwebstep <rohitwebstep@gmail.com>
2025-04-11 18:45:27 +0530

    try to fix - 13

--- src/app/supplier/category/update/page.js
+++ src/app/supplier/category/update/page.js
@@ -1,10 +1,10 @@
-import Update from '@/components/supplier/category/Update'
-import React from 'react'
+import Update from '@/components/supplier/category/Update';
+import React, { Suspense } from 'react';
 
 export default function page() {
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
             <Update />
-        </>
-    )
+        </Suspense>
+    );
 }
