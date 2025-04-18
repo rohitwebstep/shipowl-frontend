abd1538a55d7c810866296d0cc730da9d5d705ca
shikhawebtsep <shikhawebtsep@gmail.com>
2025-04-18 20:22:28 +0530

    update 7

--- src/app/dropshipping/auth/password/reset/page.js
+++ src/app/dropshipping/auth/password/reset/page.js
@@ -4,7 +4,7 @@ import {Suspense} from 'react'
 export default function page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-         <Update />
+         <ResetPassword />
        </Suspense>
   )
 }
--- src/app/supplier/auth/password/reset/page.js
+++ src/app/supplier/auth/password/reset/page.js
@@ -4,6 +4,6 @@ import {Suspense} from 'react'
 export default function page() {
   return (
  <Suspense fallback={<div>Loading...</div>}>
-      <Update />
+      <ResetPassword />
     </Suspense>  )
 }
