8d6385793ed52c3759fbbdbac2ec2f9ed94e5056
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-05 16:21:10 +0530

    update 11

--- src/app/admin/supplier/orders/page.js
+++ src/app/admin/supplier/orders/page.js
@@ -1,12 +1,15 @@
-// page.tsx
-'use client';
+'use client'; // Ensure the code runs on the client-side only
 
 import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
+// Lazy-load the Order component with Suspense
 const Order = dynamic(() => import('@/components/admin/orders/Order'), {
   suspense: true,
 });
+
+// Disable static prerendering for this page
+export const dynamic = 'force-dynamic';
 
 export default function Page() {
   return (
