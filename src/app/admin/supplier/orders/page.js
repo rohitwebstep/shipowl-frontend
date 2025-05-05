f6ad659461d75d25fedd7adc22db66fff2b098ca
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-05 16:23:14 +0530

    update 12

--- src/app/admin/supplier/orders/page.js
+++ src/app/admin/supplier/orders/page.js
@@ -8,9 +8,6 @@ const Order = dynamic(() => import('@/components/admin/orders/Order'), {
   suspense: true,
 });
 
-// Disable static prerendering for this page
-export const dynamic = 'force-dynamic';
-
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading orders...</div>}>
