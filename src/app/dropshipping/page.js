c2244b9f7b2acc5a6d93ba1c1834bedd96bf1b0c
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-05 15:40:03 +0530

    update 4

--- src/app/dropshipping/page.js
+++ src/app/dropshipping/page.js
@@ -1,10 +1,10 @@
 import Home from '@/components/dropshipping/home/Home'
-import React from 'react'
+import React,{Suspense} from 'react'
 
 export default function page() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Home />
-    </>
+    </Suspense>
   )
 }
