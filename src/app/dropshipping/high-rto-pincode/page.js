bffcd19ed1b5341fbf4164728dbc9a0e5c5541e1
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-05 16:13:46 +0530

    update 9

--- src/app/dropshipping/high-rto-pincode/page.js
+++ src/app/dropshipping/high-rto-pincode/page.js
@@ -1,10 +1,9 @@
-import HighRto from '@/components/dropshipping/high-rto/HighRto'
-import React from 'react'
+import React from 'react';
+import HighRto from '@/components/dropshipping/high-rto/HighRto';
 
-export default function page() {
-  return (
-    <>
-    <HighRto/>
-    </>
-  )
+// ✅ Disable static rendering for this page
+export const dynamic = 'force-dynamic';
+
+export default function Page() {
+  return <HighRto />;
 }
