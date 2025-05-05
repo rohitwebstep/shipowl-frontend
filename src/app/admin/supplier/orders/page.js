c59dd9bef1eb095b1c8fea4b6fffc8f0ddc9f2f9
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-05 15:36:28 +0530

    update3

--- src/app/admin/supplier/orders/page.js
+++ src/app/admin/supplier/orders/page.js
@@ -1,10 +1,17 @@
-import Order from '@/components/admin/orders/Order'
-import React from 'react'
+// page.tsx
+'use client';
 
-export default function page() {
-    return (
-        <>
-            <Order />
-        </>
-    )
+import React, { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const Order = dynamic(() => import('@/components/admin/orders/Order'), {
+  suspense: true,
+});
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading orders...</div>}>
+      <Order />
+    </Suspense>
+  );
 }
