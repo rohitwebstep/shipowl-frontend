10149cab2c1f6ce952589101a4508edbe321de4c
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 10:27:20 +0530

    updated

--- src/app/dropshipping/product/page.js
+++ src/app/dropshipping/product/page.js
@@ -1,12 +1,6 @@
-'use client'; // Required if using Suspense + dynamic in app directory
 
+import ProductDetails from '@/components/dropshipping/home/ProductDetails';
 import React, { Suspense } from 'react';
-import dynamic from 'next/dynamic';
-
-// ✅ Lazy-load the component with suspense
-const ProductDetails = dynamic(() => import('@/components/dropshipping/home/ProductDetails'), {
-  suspense: true,
-});
 
 export default function Page() {
   return (
