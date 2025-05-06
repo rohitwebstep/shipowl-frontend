4338ff55e752cbe22e373dfb5f0380d4f5b74d44
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 12:54:44 +0530

    update

--- src/app/admin/city/list/page.js
+++ src/app/admin/city/list/page.js
@@ -1,11 +1,15 @@
-import List from '@/components/admin/citymanagement/List'
-import React from 'react'
+'use client'
+
+import dynamic from 'next/dynamic';
+
+const List = dynamic(() => import('@/components/admin/citymanagement/List'), {
+  ssr: false, // Disable SSR for this component
+});
 
 export default function Page() {
   return (
     <>
-    <List/>
-    
+      <List />
     </>
-  )
+  );
 }
