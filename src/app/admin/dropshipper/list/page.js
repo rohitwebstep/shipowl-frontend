18c5b65485a5110ec6a4ad3e42075ab14c545dee
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 12:51:29 +0530

    updare

--- src/app/admin/dropshipper/list/page.js
+++ src/app/admin/dropshipper/list/page.js
@@ -1,8 +1,12 @@
-import Dropshippers from '@/components/admin/dropshipper/Dropshipper'
-import React,{Suspense} from 'react'
+import React, { Suspense } from 'react';
+
+// Dynamically import the Dropshippers component
+const Dropshippers = React.lazy(() => import('@/components/admin/dropshipper/Dropshipper'));
 
 export default function Page() {
-  return    <Suspense fallback={<div>Loading...</div>}>
-           <Dropshippers />
-     </Suspense>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Dropshippers />
+    </Suspense>
+  );
 }
--- src/app/admin/supplier/orders/page.js
+++ src/app/admin/supplier/orders/page.js
@@ -1,8 +1,12 @@
-import Orders from '@/components/admin/orders/Order';
 import React, { Suspense } from 'react';
+
+// Dynamically import the Orders component
+const Orders = React.lazy(() => import('@/components/admin/orders/Order'));
+
 export default function Page() {
-return (
+  return (
     <Suspense fallback={<div>Loading...</div>}>
       <Orders />
     </Suspense>
-  );}
+  );
+}
