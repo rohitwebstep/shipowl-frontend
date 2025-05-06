1dfe091bdb26f826e57957d894d3e82a37c33f98
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 12:20:40 +0530

    ll

--- src/app/dropshipping/product-details/page.js
+++ src/app/dropshipping/product-details/page.js
@@ -1,8 +1,16 @@
-import ProductDetails from '@/components/dropshipping/home/ProductDetails'
-import React from 'react'
+"use client"
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import ProductDetails with ssr: false to disable SSR for this component
+const ProductDetails = dynamic(() => import('@/components/dropshipping/home/ProductDetails'), { 
+  ssr: false // This disables SSR and renders it only on the client
+});
 
 export default function Page() {
   return (
-    <><ProductDetails/></>
-  )
+    <div>
+      <ProductDetails />
+    </div>
+  );
 }
