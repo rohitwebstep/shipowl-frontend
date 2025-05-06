ee3fbc4580dcfc845755ab56b81db2255623ee0d
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 10:15:12 +0530

    update 3

--- src/app/dropshipping/manage-orders/page.js
+++ src/app/dropshipping/manage-orders/page.js
@@ -1,10 +1,13 @@
 import ManageOrders from "@/components/dropshipping/manageorders/ManageOrders";
-
+import React, {Suspense} from React
 export default function Page() {
 
   return (
     <>
+     <Suspense fallback={<div>Loading...</div>}>
+    
     <ManageOrders/>
+       </Suspense>
     </>
   )
 }
