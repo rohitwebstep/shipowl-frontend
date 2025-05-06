8c26f3e33d7ca73f174fcbe65b7f3c47d66269c3
shikhawebtsep <shikhawebtsep@gmail.com>
2025-05-06 10:30:54 +0530

    update

--- src/app/dropshipping/product/source/create/page.js
+++ src/app/dropshipping/product/source/create/page.js
@@ -1,6 +1,10 @@
-import SourceProductForm from '@/components/dropshipping/product/SourceProductForm'
+import SourceProductForm from '@/components/dropshipping/product/SourceProductForm';
+import React, { Suspense } from 'react';
+
 export default function page() {
   return (
+   <Suspense fallback={<div>Loading...</div>}>
    <SourceProductForm/>
+   </Suspense>
   )
 }
--- src/app/dropshipping/product/source/page.js
+++ src/app/dropshipping/product/source/page.js
@@ -1,11 +1,11 @@
 import ProductList from '@/components/dropshipping/product/ProductList'
-import React from 'react'
+import React, { Suspense } from 'react';
 
 function page() {
   return (
-    <>
+   <Suspense fallback={<div>Loading...</div>}>
     <ProductList/>
-    </>
+   </Suspense>
   )
 }
 
